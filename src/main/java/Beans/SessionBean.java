90557e0612c32414a5f32c62840a4e9efabe799a
FMinus <ayoub.deqqaq@gmail.com>
2016-05-11 21:04:43 +0100

    added : loginFilter sessionLoginListner mongo login gueries return first result only logged in user's credentials loaded in the session uploads stores and uses avatars

---
+++ src/main/java/Beans/SessionBean.java
@@ -0,0 +1,27 @@
+package Beans;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class SessionBean
+{
+     public static HttpSession getSession() 
+     {
+        return (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+     }
+     
+     public static HttpServletRequest getRequest() 
+     {
+        return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+     }
+     
+      public static Object getCurrentUser() 
+      {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false); 
+        return session.getAttribute("currentUser");
+        
+      }
+}
+
+
--- src/main/java/Controllers/Etudiant/HomeEtudiant.java
+++ src/main/java/Controllers/Etudiant/HomeEtudiant.java
@@ -5,27 +5,31 @@
 */
 package Controllers.Etudiant;
 
-import Controllers.SessionLoginListener;
-import Entities.EtudiantEntity;
+import Beans.SessionBean;
 import Metier.Etudiant;
 import java.io.Serializable;
-import java.security.Principal;
-import java.util.List;
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 
 @ManagedBean
 @SessionScoped
-public class HomeEtudiant extends SessionLoginListener implements Serializable
+public class HomeEtudiant implements Serializable
 {
     
+    @Inject Etudiant etudiant;
     
-    @Inject
-    Etudiant etudiant;
+    public Etudiant getEtudiant()
+    {
+        return etudiant;
+    }
+    
+    public void setEtudiant(Etudiant etudiant)
+    {
+        this.etudiant = etudiant;
+    }
+    
     
     private String recherche;
     
@@ -46,10 +50,7 @@ public class HomeEtudiant extends SessionLoginListener implements Serializable
     @PostConstruct
     public void onLoad()
     {
-        FacesContext context = FacesContext.getCurrentInstance();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-            
-        etudiant = (Etudiant) request.getSession().getAttribute("userEtudiant");
+        etudiant = (Etudiant) SessionBean.getCurrentUser();
        
     }
     
@@ -63,61 +64,19 @@ public class HomeEtudiant extends SessionLoginListener implements Serializable
         
     }
 
-    public Etudiant getEtudiant()
-    {
-        return etudiant;
-    }
-
-    public void setEtudiant(Etudiant etudiant)
-    {
-        this.etudiant = etudiant;
-    }
+   
     
     
-    
-    
-    public HttpServletRequest getHttpServletRequest()
-    {
-        FacesContext context = FacesContext.getCurrentInstance();
-        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
-        
-        return request;
-    }
     
     public String getInfoEtudiant()
     {
-        /*
-        HttpServletRequest request = getHttpServletRequest();
-        
-        etudiant = (Etudiant) request.getSession().getAttribute("currentUser");
-        
-        if(etudiant != null)
-        {
-            System.out.println("constructor Home Etudiant : "+etudiant.toString());
-            return etudiant.toString();
-        }
-        return "-------------------- bean resolve fail! ------------------";   
-        
-        //
-        */
+        if(etudiant!=null)
+            System.out.println(" HomeEtudiant :  "+etudiant);
         
         return etudiant == null ? etudiant.toString() : "-------------------- bean resolve fail! ------------------";
     }
     
-    public Etudiant getSessionEtudiant()
-    {
-        
-        
-        HttpServletRequest request = getHttpServletRequest();
-        
-        etudiant = (Etudiant) request.getSession().getAttribute("currentUser");
-        
-        
-        if(etudiant!=null)
-           System.out.println("Session get : "+etudiant);
-        
-        return etudiant;
-    }
+    
     
     public String rechercheInfo()
     {
@@ -126,23 +85,10 @@ public class HomeEtudiant extends SessionLoginListener implements Serializable
     
     public String avatarGetter()
     {
-        /*
-        if(getSessionEtudiant().getUrlAvatar() == null)
+        if(this.etudiant== null || !this.etudiant.getUrlAvatar().contains("jpg") || !this.etudiant.getUrlAvatar().contains("png") || !this.etudiant.getUrlAvatar().contains("jpeg") || !this.etudiant.getUrlAvatar().contains("gif"))
             return "/resources/images/avatars/nophoto.png";
         else
-        {
-            System.out.println("/resources/images/avatars/"+getSessionEtudiant().getUrlAvatar());
-            return "/resources/images/avatars/"+getSessionEtudiant().getUrlAvatar();
-        }*/
-        System.out.println("Avatar to get : "+etudiant.getUrlAvatar());
-        
-        //if(this.etudiant.getUrlAvatar() == null)
-        //    return "/resources/images/avatars/nophoto.png";
-        //else
-        //{
-            System.out.println("/resources/images/avatars/"+getSessionEtudiant().getUrlAvatar());
             return "/resources/images/avatars/"+etudiant.getUrlAvatar();
-       // }
         
     }
     
